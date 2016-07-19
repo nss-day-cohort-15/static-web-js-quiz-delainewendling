0802753561a32363a610b9ab178157c0aff42ec8
Delaine Wendling <delaine.wendling@gmail.com>
2016-07-19 15:21:09 -0500

    Added an event listener to the enter key

--- quiz.js
+++ quiz.js
@@ -3,19 +3,12 @@ var height = document.getElementById("height");
 var character = document.getElementById("character");
 
 function growTree(object) {
-  for(i=0; i<=tree.treeHeight; i++) {
+  for(i=0; i<tree.treeHeight; i++) {
     var space=" ";
     console.log(space.repeat(tree.treeHeight-(i-1))+tree.treeChar.repeat(2*i+1));
   }
 }
 
-// function growTree() {
-//   for(i=0; i<=height; i++) {
-//     var oneLess = i-1;
-//     var space = " ";
-//     console.log(space.repeat(height-oneLess)+character.repeat(2*i+1));
-//   }
-// }
 document.getElementById("grow").addEventListener("click", function(event) {
   event.preventDefault();
   console.log(height.value);
@@ -29,3 +22,26 @@ document.getElementById("grow").addEventListener("click", function(event) {
   height.value = "";
   character.value = "";
 });
+
+var theParent = document.getElementById("formContainer");
+theParent.addEventListener("keypress", submitForm, false);
+
+function submitForm(event) {
+    if (event.target !== event.currentTarget) {
+        var pressedItem = event.target.id;
+    }
+    var key = event.which || event.keyCode;
+    if (key === 13) {
+        if(height.value !== "" && character.value !== "") {
+          tree.treeHeight = Number(height.value);
+          tree.treeChar = character.value;
+          growTree(tree);
+        } else {
+          alert("Both fields must have a value");
+        }
+        height.value = "";
+        character.value = "";
+    }
+    event.stopPropagation();
+}
+
