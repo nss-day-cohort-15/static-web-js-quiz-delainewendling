1d0e330381439c6ee0fcd34744e69b2924702ae5
Delaine Wendling <delaine.wendling@gmail.com>
2016-07-19 14:53:12 -0500

    Created an object rather than variables and created an alert when fields are empty

--- quiz.js
+++ quiz.js
@@ -1,14 +1,31 @@
+var tree = {};
+var height = document.getElementById("height");
+var character = document.getElementById("character");
 
-function growTree() {
-  var height = Number(document.getElementById("height").value);
-  var character = document.getElementById("character").value;
-  for(i=0; i<=height; i++) {
-    var oneLess = i-1;
-    var space = " ";
-    console.log(space.repeat(height-oneLess)+character.repeat(2*i+1));
+function growTree(object) {
+  for(i=0; i<=tree.treeHeight; i++) {
+    var space=" ";
+    console.log(space.repeat(tree.treeHeight-(i-1))+tree.treeChar.repeat(2*i+1));
   }
 }
+
+// function growTree() {
+//   for(i=0; i<=height; i++) {
+//     var oneLess = i-1;
+//     var space = " ";
+//     console.log(space.repeat(height-oneLess)+character.repeat(2*i+1));
+//   }
+// }
 document.getElementById("grow").addEventListener("click", function(event) {
   event.preventDefault();
-  growTree();
+  console.log(height.value);
+  if (height.value !== "" && character.value !== "") {
+    tree.treeHeight = Number(height.value);
+    tree.treeChar = character.value;
+    growTree(tree);
+  } else {
+    alert("Both fields must have a value");
+  }
+  height.value = "";
+  character.value = "";
 });
